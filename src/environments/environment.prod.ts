a8ea504eb8fea398b3a06195df509cd12cdb7d6d
Taranjot <ktaranjot501@gmail.com>
2026-06-08 23:38:54 +0530

    firebase connection improved

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
 
   firebaseConfig: {
-    apiKey: "AIzaSyCMdTv3JngtJpfRGXiRdRzbXPaD55qhdRQ",
-    authDomain: "knowledge-hub-24183.firebaseapp.com",
-    projectId: "knowledge-hub-24183",
-    storageBucket: "knowledge-hub-24183.firebasestorage.app",
-    messagingSenderId: "152001840741",
-    appId: "1:152001840741:web:2dfb10aecba57d4487e88c"
+  apiKey: "AIzaSyCMdTv3JngtJpfRGXiRdRzbXPaD55qhdRQ",
+  authDomain: "knowledge-hub-24183.firebaseapp.com",
+  projectId: "knowledge-hub-24183",
+  storageBucket: "knowledge-hub-24183.firebasestorage.app",
+  messagingSenderId: "152001840741",
+  appId: "1:152001840741:web:2dfb10aecba57d4487e88c"
   }
 };
